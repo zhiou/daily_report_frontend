e11de49c2dcfe0a1737130684811ae4bcd0767ce
zhiou <myself@zhouhuang.me>
2021-12-03 17:39:20 +0800

    - 实现周报排序

--- src/api/request/index.js
+++ src/api/request/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: zhiou
  * @Date: 2021-09-26 10:33:57
- * @LastEditTime: 2021-11-16 15:49:21
+ * @LastEditTime: 2021-12-03 16:04:52
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /daily-report-frontend/src/request/index.js
@@ -20,13 +20,13 @@ const service = axios.create({
   timeout: 10000,
 });
 
-MockConfig.mocks.forEach((config) => {
-  if (config.enabled) {
-    Mock.mock(config.api, config.method, () => {
-      return config.data
-    });
-  }
-});
+// MockConfig.mocks.forEach((config) => {
+//   if (config.enabled) {
+//     Mock.mock(config.api, config.method, () => {
+//       return config.data
+//     });
+//   }
+// });
 
 service.interceptors.request.use(
   (request) => {
