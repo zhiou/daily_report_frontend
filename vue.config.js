1dc7ea2253ae8b5b4f41c1b00b942287d0cb7fb8
zhiou <myself@zhouhuang.me>
2022-02-23 10:22:55 +0800

    防止浏览器缓存

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
+const Timestamp = new Date().getTime();
+
 module.exports = {
   productionSourceMap: false,
   configureWebpack: {
-    devtool: 'source-map'
+    devtool: 'source-map',
+    output: { // 输出重构 打包编译后的 文件名称 【模块名称.版本号.时间戳】
+      filename: `static/js/[name].${process.env.VUE_APP_VERSION}.${Timestamp}.js`,
+      chunkFilename: `static/js/[name].${process.env.VUE_APP_VERSION}.${Timestamp}.js`
+    },
   }
 }
